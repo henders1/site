c3492fe4a128059a81406ae3a226cc66087aa2e3
henders <148650724+henders1@users.noreply.github.com>
2025-09-07 22:15:55 +0200

    Update title.js

--- js/title.js
+++ js/title.js
@@ -1,5 +1,5 @@
         // Texto que será exibido no <title>
-        const titleText = "@biaquista";
+        const titleText = "@henders";
         let index = 0;
 
         // Função para animar o título
